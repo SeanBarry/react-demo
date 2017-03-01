46adea569b027ab1da399def78d1a67acf450295
Sean Barry <se@nbarry.co.uk>
2017-03-01 14:34:01 +0000

    Add Delete Functionality

--- client/main.js
+++ client/main.js
@@ -10,7 +10,6 @@ Template.MessageList.helpers({
 });
 
 
-
 Template.Message.helpers({
   formatDate(date) {
     return moment(date).format('ddd, H:mma');
@@ -18,9 +17,31 @@ Template.Message.helpers({
 });
 
 
+Template.Message.onRendered(() => {
+  console.log('Message has been rendered!');  
+});
+
+
+Template.Message.onDestroyed(() => {
+  console.log('Message has been destroyed!');  
+});
+
+
+Template.Message.events({
+  'click .delete': function(event) {
+    event.preventDefault();
+    const messageId = this._id;
+
+    Messages.remove({
+      '_id': messageId
+    });
+
+  },
+});
+
 
 Template.SubmitMessage.events({
-  'submit #sendMessage'(event) {
+  'submit #sendMessage':(event) => {
   	event.preventDefault();
   	const message = event.target.message.value;
 
